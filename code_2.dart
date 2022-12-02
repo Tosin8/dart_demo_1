c0c884f5648911b9771c8a493349a98f4a42930d
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-12-02 23:37:30 +0100

    Update code_2.dart

--- code_2.dart
+++ code_2.dart
@@ -2,13 +2,24 @@
 
 // 1. ASYNCHORNOUS PROGRAMMING - STREAMS, It is c
 
-Future<int> sumStream(stream<int> stream) async {
+Future<int> sumStream(Stream<int> stream) async {
   var sum = 0;
 
   await for (final value in stream) {
     sum += value;
   }
-  return sum; 
+  return sum;
 }
 
-Stream<int> countStrearm
+Stream<int> countStream(int to) async* {
+  for (int i = 1; i <= to; i++) {
+    yield i;
+  }
+}
+
+// ignore: unused_element
+void main() async {
+  var stream = countStream(10);
+  var sum = await sumStream(stream);
+  print(sum);
+}
