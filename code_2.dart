0a4c3a08541df14faabe3a4aa85af7c99cc4d595
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-12-02 23:33:46 +0100

    Update code_2.dart

--- code_2.dart
+++ code_2.dart
@@ -1,5 +1,14 @@
-// source: from dart official 
-
+// source: from dart official
 
 // 1. ASYNCHORNOUS PROGRAMMING - STREAMS, It is c
 
+Future<int> sumStream(stream<int> stream) async {
+  var sum = 0;
+
+  await for (final value in stream) {
+    sum += value;
+  }
+  return sum; 
+}
+
+Stream<int> countStrearm
