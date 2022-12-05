10bd38ac99d92393ff9966140d9232aed21e3c1c
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-12-05 20:12:30 +0100

    Update dart_tour.dart

--- dart_tour.dart
+++ dart_tour.dart
@@ -1,10 +1,12 @@
 // 1. BASIC DART PROGRAM
 
 void printInteger(int aNumber) {
+  // define a function
   print('The number is $aNumber.');
 }
 
 void main() {
+  // point where the app starts executing
   var number = 42;
   printInteger(number);
 }
